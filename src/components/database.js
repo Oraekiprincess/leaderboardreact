344f86c4c88acdd631208cb0df0b5689972a8146
Oraekiprincess <114993346+Oraekiprincess@users.noreply.github.com>
2024-03-25 19:32:26 +0100

    Add files via upload

---
+++ src/components/database.js
@@ -0,0 +1,44 @@
+export const Leaderboard = [
+    {
+        name: "Prince Oraeki",
+        location: "Enugu",
+        score : 1550,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-02-10"
+    },
+    {
+        name: "Boaz Oraeki",
+        location: "Awka",
+        score : 2310,
+        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-01-01"
+    },
+    {
+        name: "Hilda Akachukwu",
+        location: "Lagos",
+        score : 350,
+        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-08-17"
+    },
+    {
+        name: "Adella Wunsch",
+        location: "South Africa",
+        score : 2100,
+        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2021-10-23"
+    },
+    {
+        name: "Brian Ayo",
+        location: "Lagos",
+        score : 1250,
+        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-01-22"
+    },
+    {
+        name: "Kameron Cole",
+        location: "Abuja",
+        score : 5250,
+        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        dt: "2022-01-21"
+    }
+]
