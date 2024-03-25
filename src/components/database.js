50ceafb11e04de01e9d717114875d6bf42a173c1
Oraekiprincess <114993346+Oraekiprincess@users.noreply.github.com>
2024-03-25 21:05:53 +0100

    Update database.js

--- src/components/database.js
+++ src/components/database.js
@@ -1,6 +1,6 @@
 export const Leaderboard = [
     {
-        name: "Prince Oraeki",
+        name: "Princess Oraeki",
         location: "Enugu",
         score : 1550,
         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
